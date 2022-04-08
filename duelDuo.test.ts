6a9a12831c5431ea93cc04b33aca85d6cb5c65f2
Amro Abdalla <wed.kom@hotmail.com>
2022-04-08 11:53:48 -0500

    additional commit

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -18,3 +18,18 @@ test('Title shows up when page loads', async () => {
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
 })
+
+test('The page should display choices ', async () => {
+    await driver.findElement(By.id('draw'))
+    const divs = await driver.findElements(By.className('bot-card-outline'));
+    const displayed = await divs.includes;
+    expect(displayed).toBe(true)
+})
+
+
+test('The page should display choices ', async () => {
+    await driver.findElements(By.className('bot-btn'))[0]
+    const divs = await driver.findElements(By.className('bot-card-outline'))[0];
+    const displayed = await divs.includes;
+    expect(displayed).toBe(true)
+})
